1a5e62622533825f0584fd6d9b18920897f8826a
auplt <maxapple2012@mail.ru>
2022-02-21 01:38:36 +0300

    task 4 done

--- task_4/solution.js
+++ task_4/solution.js
@@ -3,10 +3,25 @@
 function parseProducts(json) {
     // Аргументом функции является JSON
 
+    let data;
+    data = JSON.parse(json);
+    let products;
+    products = data.products;
+    return products;
     // Преобразуйте строку json, переданную как аргумент функции,
     // в объект с помощью функции JSON.parse(json)
     // и запишите в переменную data
     // Верните как результат функции свойство products объекта data
+}
+
+function renderProductsCards(json) {
+    clearProducts();
+    let products;
+    products = parseProducts(json);
+    let length = products.length;
+    for (let i = 0; i < length; i++) {
+        addProduct(products[i]);
+    }
 }
 
 // Напишите функцию renderProductsCards(json)
